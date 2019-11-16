50f64fc661084402c1d02d4f67ec703f98131ca9
Akihiro YOSHII <akihiro.441.r@gmail.com>
2019-11-16 16:32:08 +0900

    Update scripts

--- bitflyer_test/src/candle2picture.py
+++ bitflyer_test/src/candle2picture.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import mpl_finance as mpf
+import argparse
 
 import matplotlib
 import matplotlib.dates as mdates
@@ -7,40 +8,62 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt  # NOQA
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=str, help="path of candle csv")
+    parser.add_argument("output", type=str, help="path of graph")
+
+    parser.add_argument("--output_length", type=int, default=100,
+                        help="length of candle data output. If 0, print all.")
+    parser.add_argument("--roll_spans", type=int, nargs="+",
+                        default=[5, 15, 60], help="Plot rolling means")
+    parser.add_argument("--plot_candle", action="store_true")
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
+
     # read
-    column_names = ('time', 'open', 'close', 'high', 'low')
-    df = pd.read_csv('test.csv', names=column_names, index_col=0,
-                     parse_dates=True)
+    df = pd.read_csv(args.input, index_col=0, parse_dates=True)
+    df.index = pd.to_datetime(df.index)
+    df.index = mdates.date2num(df.index)
 
     # rolling_average
-    df['roll_5'] = df['close'].rolling(5).mean()
-    df['roll_25'] = df['close'].rolling(25).mean()
-    df['roll_75'] = df['close'].rolling(75).mean()
+    for span in args.roll_spans:
+        name = 'roll_{}'.format(span)
+        df[name] = df['close'].rolling(span).mean()
 
-    df_ = df.copy()
-    df_.index = mdates.date2num(df_.index)
-    ohlc = df_.tail(100).reset_index().values
+    if args.output_length > 0:
+        df = df.tail(args.output_length)
 
     # Plot Figure
-    fig = plt.figure()
+    fig = plt.figure(figsize=(16.0, 9.0))
     ax = plt.subplot()
 
-    mpf.candlestick_ochl(ax, ohlc, colorup='g', colordown='r',
-                         width=0.03, alpha=0.75)
+    # Plot candle
+    if args.plot_candle:
+        ohlc = df.reset_index().values
+        width = (ohlc[1][0] - ohlc[0][0]) * 0.7
+        mpf.candlestick_ochl(ax, ohlc, colorup='g', colordown='r',
+                             width=width, alpha=0.75)
+    else:
+        ax.plot(df.index, df.close, color="black", linewidth=0.7)
 
-    ax.plot(ohlc[:, 0], ohlc[:, 6])
-    ax.plot(ohlc[:, 0], ohlc[:, 7])
-    ax.plot(ohlc[:, 0], ohlc[:, 8])
+    # Plot rolling means
+    for span in args.roll_spans:
+        name = "roll_{}".format(span)
+        ax.plot(df.index, getattr(df, name), linewidth=0.7, label=name)
 
-    ax.grid()
+    ax.grid(linestyle=':')
+    ax.legend()
 
     locator = mdates.AutoDateLocator()
     ax.xaxis.set_major_locator(locator)
-    ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(locator))
-    fig.autofmt_xdate()  # format x pole
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%y/%m/%d %H:%M'))
+    # fig.autofmt_xdate()  # format x pole
 
-    fig.savefig("test.png")
+    fig.savefig(args.output)
 
 
 if __name__ == '__main__':
--- bitflyer_test/src/executions_to_minute_data.py
+++ bitflyer_test/src/executions_to_minute_data.py
@@ -1,13 +1,20 @@
 from collections import deque
 import pickle
 import datetime
+import argparse
+import pandas as pd
+import glob
 
 import timer_functions
 
-from analyze import TimeData
 
-import csv
-import glob
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output", type=str, help="path of candle csv")
+
+    parser.add_argument("--span", type=int, default=1,
+                        help="Squash minutes, default=1")
+    return parser.parse_args()
 
 
 def open_executions(path):
@@ -16,15 +23,8 @@ def open_executions(path):
     return executions
 
 
-# def pickup_term(executions, start, end):
 def pickup_term(executions, end):
     target = deque()
-    # for e in executions:
-    #     tmp_dt = e.exec_date
-    #     if start < tmp_dt < end:
-    #         target.append(e)
-    #     else:
-    #         continue
     while len(executions) > 0:
         if executions[0].exec_date < end:
             target.append(executions.popleft())
@@ -35,65 +35,79 @@ def pickup_term(executions, end):
 
 
 def summary_executions(executions):
-    open_price = executions[0].price
+    open = executions[0].price
     close = executions[-1].price
-    low = min(executions, key=lambda e: e.price).price
     high = max(executions, key=lambda e: e.price).price
+    low = min(executions, key=lambda e: e.price).price
     volume = 0.0
     for e in executions:
         volume += e.size
 
-    return open_price, close, low, high, volume
+    return open, close, high, low, volume
 
 
 def main():
+    args = parse_args()
+
     # Open pkl
     executions = deque()
     files = glob.glob('data/executions/*')
     for f in files:
         executions.extend(open_executions(f))
+    print("LOAD EXECUTIONS")
 
     # Sort executions
     executions = list(executions)
     executions.sort(key=lambda e: e.exec_date)
     executions = deque(executions)
 
+    print("SORT EXECUTIONS")
+
     # Loop
-    span = 60  # minutes
     start = timer_functions.mask_time(executions[0].exec_date, mask="seconds")
-    end = start + datetime.timedelta(minutes=span)
+    span = datetime.timedelta(minutes=args.span)
+    end = start + span
 
     # pickup from list
-    tds = []
-    while end < timer_functions.mask_time(
-            executions[-1].exec_date) + datetime.timedelta(minutes=1):
-        # target = pickup_term(executions, start, end)
+    starts = []
+    opens = []
+    closes = []
+    highs = []
+    lows = []
+    volumes = []
+    while len(executions) > 0:
+        if (timer_functions.mask_time(
+                executions[-1].exec_date) + span) < start:
+            print("???")
+            break
         target = pickup_term(executions, end)
 
+        # Get open/close/high/low/volume
         if len(target) > 0:
-            # Get open/close/low/high/volume
-            open_price, close, low, high, volume = summary_executions(target)
+            open, close, high, low, volume = summary_executions(target)
         else:
-            open_price = close
-            low = close
-            high = close
+            open = high = low = close
             volume = 0.0
 
-        td = TimeData(open=open_price, close=close, low=low, high=high,
-                      start=start, end=end, volume=volume)
-        print(td)
-
-        tds.append(td)
+        starts.append(start)
+        opens.append(open)
+        closes.append(close)
+        highs.append(high)
+        lows.append(low)
+        volumes.append(volume)
 
         start = end
-        end = start + datetime.timedelta(minutes=span)
+        end = start + span
+
+    df = pd.DataFrame(
+        index=starts,
+        columns=["open", "close", "high", "low", "volume"],
+        data={"open": opens, "close": closes,
+              "high": highs, "low": lows, "volume": volumes}
+    )
 
     # Save Info
-    with open('test.csv', 'w') as f:
-        writer = csv.writer(f)
-        for td in tds:
-            writer.writerow([td.start, td.open, td.close,
-                             td.high, td.low, td.volume])
+    df.to_csv(args.output)
 
 
 if __name__ == '__main__':
